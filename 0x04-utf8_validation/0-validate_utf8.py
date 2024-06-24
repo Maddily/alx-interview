20c775fb7f39e21badd72ad2fe1c54970f9cbfbc
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-24 20:23:33 +0300

    Validate if a dataset represents valid utf-8 encoding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module contains a function to validate whether a given
+list of integers represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    Check if the given list of integers represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing a UTF-8 encoding.
+
+    Returns:
+        bool: True if the encoding is valid, False otherwise.
+    """
+
+    n = len(data)
+    i = 0
+
+    while i < n:
+        leader_byte = data[i]
+
+        if leader_byte not in range(0, 256):
+            return False
+
+        # Determine the number of bytes in a sequence
+        if leader_byte in range(0, 128):
+            number_of_bytes = 1
+        elif leader_byte in range(192, 224):
+            number_of_bytes = 2
+        elif leader_byte in range(224, 240):
+            number_of_bytes = 3
+        elif leader_byte in range(240, 248):
+            number_of_bytes = 4
+        else:
+            return False
+
+        # Check if the remaining bytes are enough to complete a sequence
+        if i + number_of_bytes > n:
+            return False
+
+        # Check trailing bytes
+        for j in range(1, number_of_bytes):
+            if data[i + j] not in range(128, 192):
+                return False
+
+        i += number_of_bytes
+
+    return True
